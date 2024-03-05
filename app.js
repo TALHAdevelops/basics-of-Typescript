a0dbcea8b939db52ac309fe01261f007adc5858a
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-05 20:51:27 +0500

    .ts /.js files

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log('Player just get 5 points.');
+}
+if (alien_color == "blue") {
+    console.log('no points');
+}
---
+++ app.ts
@@ -0,0 +1,8 @@
+let alien_color="green";
+
+if (alien_color=="green"){
+    console.log('Player just get 5 points.');
+}
+if (alien_color=="blue") {
+    console.log('no points');
+}
